c4b59d276f0975fcf03fd625aa2705237a767580
Kacper <kacper.m.wojtowicz@gmail.com>
2022-06-28 11:45:41 +0200

    explore view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from './component/PrivateRoute';
 import Explore from './pages/Explore';
+import Category from './pages/Category';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import Signin from './pages/Signin';
@@ -16,6 +17,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Explore />}></Route>
+          <Route path='/category/:categoryName' element={<Category />}></Route>
           <Route path='/offers' element={<Offers />}></Route>
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />}></Route>
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import shoesCategoryImage from '../assets/img/eebec51f.jpeg';
+import hatsCategoryImage from '../assets/img/6e5630db.jpg';
+import sunglassesCategoryImage from '../assets/img/312272.jpeg';
+import othersCategoryImage from '../assets/img/7ff39960.jpeg';
 
 export default function Explore() {
   return (
-    <div>Explore</div>
-  )
+    <div className='explore'>
+      <header>
+        <p className='pageHeader'>Explore</p>
+      </header>
+
+      <main>
+        <p className='exploreCategoryHeading'>Categories</p>
+        <div className='exploreCategories'>
+          <Link to='/category/shoes'>
+            <img
+              src={shoesCategoryImage}
+              alt='shoes'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Shoes</p>
+          </Link>
+          <Link to='/category/hats'>
+            <img
+              src={hatsCategoryImage}
+              alt='hats'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Hats</p>
+          </Link>
+          <Link to='/category/sunglasses'>
+            <img
+              src={sunglassesCategoryImage}
+              alt='sunglasses'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Sunglasses</p>
+          </Link>
+          <Link to='/category/others'>
+            <img
+              src={othersCategoryImage}
+              alt='others'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Others</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 }
